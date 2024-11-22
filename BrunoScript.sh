60328caee6d73f903d7f69dfa55b8d59c7a45ba1
David Nowak <david.nowak@consort-group.com>
2024-11-22 09:56:42 +0100

    Initial commit

---
+++ BrunoScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "C:\Users\david.nowak\BrunoCollection\Fakestore API"
+bru run GetFakestoreapiSchemaValidation.bru --env QA --output "C:\Users\david.nowak\BrunoCollection\Fakestore API\results.json" --format json
